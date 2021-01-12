13a39cb52afbfdf94c356a05d9a806731dee379b
Ayushi <ayushigoyal2840@gmail.com>
2021-01-12 16:20:31 +0530

    created a new oage for new contaacts

--- contact-app/src/components/contacts/Addcontacts.js
+++ contact-app/src/components/contacts/Addcontacts.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Addcontacts = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
     return (
         <div className="card border-0 shadow">
             <div className="card-header">
-                Ayushi welcome
+                Create New Contact
+                <div className="card-body">
+                    <form>
+                        <div className="form-group">
+                            <input type="text"
+                            className="form-control"
+                            placeholder="Enter Your Name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                             />
+                             </div>
+                        <div className="form-group">
+                            <input type="text"
+                            className="form-control"
+                            placeholder="Enter Your Phone Number"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                             />
+                             </div>
+                        <div className="form-group">
+                            <input type="text"
+                            className="form-control"
+                            placeholder="Enter Your Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                             />
+                        </div>
+                        <button className="btn btn-primary" type="submit">Create Contact</button>
+                    </form>
+                </div>
             </div>
            
         </div>
