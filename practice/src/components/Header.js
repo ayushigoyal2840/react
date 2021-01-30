aaa62b70a345176af5e49b6b93a44f5fa83bd0d7
Ayushi <ayushigoyal2840@gmail.com>
2021-01-30 18:21:50 +0530

    made navcar and added a jumbotron

---
+++ practice/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="jumbotron">
+        <h1 className="display-1">React</h1>
+        <h1 className="text">A JavaScript library for building user interfaces</h1>
+        <button className="button">Get Started</button>
+        <a href="#" className="link">Take the tutorial</a>
+        
+        </div>        
+    )
+}
+
+export default Header
