3b54f1dc457696f15607bf2e30b6755a2e532554
Ayushi <ayushigoyal2840@gmail.com>
2021-02-03 22:14:55 +0530

    some improvement in desgning

--- practice/src/components/Para.js
+++ practice/src/components/Para.js
@@ -6,7 +6,7 @@ function Para() {
         // <div className="flex-container">
             <div class="flex-container" >
     <div className="box">
-    <h4>Declarative</h4>
+    <h4 className="heading">Declarative</h4>
     <p class="paragraph">React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
 
 Declarative views make your code more predictable and easier to debug.</p>
@@ -14,14 +14,15 @@ Declarative views make your code more predictable and easier to debug.</p>
     
    
   
-    <div>
-    <h4>Declarative</h4>
+    <div className="box">
+    <h4 className="heading">Declarative</h4>
+
     <p class="paragraph">React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
 
 Declarative views make your code more predictable and easier to debug.</p>
     </div>
-  <div>
-  <h4>Declarative</h4>
+  <div className="box">
+  <h4 className="heading">Declarative</h4>
     <p class="paragraph">React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
 
 Declarative views make your code more predictable and easier to debug.</p>
