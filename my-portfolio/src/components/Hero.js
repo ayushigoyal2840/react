66e48c977826101fd3da003db8dc5e41e2487985
Ayushi <ayushigoyal2840@gmail.com>
2021-02-15 18:15:12 +0530

    added animation in images

--- my-portfolio/src/components/Hero.js
+++ my-portfolio/src/components/Hero.js
@@ -1,23 +1,29 @@
 import React,{useRef,useEffect} from 'react';
-import {TweenMax} from 'gsap';
+import {TweenMax, TimelineLite, Power3} from 'gsap';
 import ayu1 from './hero1.jpeg';
 import ayu2 from './hero2.jpg'
 
 const Hero = () => {
   let app = useRef(null);
   let images = useRef(null);
-
+  let tl = new TimelineLite()
 
   useEffect(() => {
-    const ayu1 = images.firstElementChild;
-    const ayu2 = images.lastElementChild;
+    const img1 = images.firstElementChild;
+    const img2 = images.lastElementChild;
 
 
 
 
     TweenMax.to(app, 0, {css: {visibility: 'visible'}})
     // console.log(app);
-    console.log(ayu1,ayu2);
+    // console.log(ayu1,ayu2);
+
+    tl.from(img1, 1.2, {y: 1280, ease: Power3.easeOut})
+    .from(img1.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
+    .from(img2, 1.2, {y: 1280, ease: Power3.easeOut}, .2)
+    .from(img2.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
+   
 
   })
     return (
@@ -60,9 +66,9 @@ with Webflow.            </p>
 
          </div>
          <div className="hero-images">
-          <div className="hero-images-inner">
+          <div className="hero-images-inner" ref={el => images = el}>
             
-            <div className="hero-image ayu1" ref={el => images = el}>
+            <div className="hero-image ayu1">
               <img src={ayu1}></img>
             </div>
             <div className="hero-image ayu2">
