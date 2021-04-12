973074e3f2343ea4737efa0ca56ac51d00e153ca
Ayushi <ayushigoyal2840@gmail.com>
2021-04-12 19:39:27 +0530

    added some things

---
+++ practice-react/src/components/CourseStructure.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import {Link} from 'react-router-dom';
+
+function CourseStructure(props) {
+   console.log(props);
+  return (
+    <div>
+        <h1>Welcome to course</h1>
+    </div>
+  );
+}
+
+export default CourseStructure;
